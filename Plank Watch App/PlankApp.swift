5f4c564383a9d123ec1ebc5d01f5f2059db16785
Donguk Kim <ehddnr73@naver.com>
2023-03-23 13:47:57 +0900

    Plank, watchOS project 생성

---
+++ Plank Watch App/PlankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlankApp.swift
+//  Plank Watch App
+//
+//  Created by 김동욱 on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Plank_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
