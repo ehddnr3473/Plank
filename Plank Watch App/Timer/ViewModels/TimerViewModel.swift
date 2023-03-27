e685986a185a5b9149add24324a99cc66b734640
Donguk Kim <ehddnr73@naver.com>
2023-03-27 19:00:31 +0900

    import UserNotifications
    - configureNofitication():  UNNotificationSettings를 통해 Notification 허용 상태를 확인
    - requestAuthorization(): 여러가지 Notification 옵션에 대해 승인 요청

--- Plank Watch App/Timer/ViewModels/TimerViewModel.swift
+++ Plank Watch App/Timer/ViewModels/TimerViewModel.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
+import UserNotifications
 
 protocol TimerViewModelInput: AnyObject {
+    func configureNotification()
     func startTimer()
     func tick()
 }
@@ -32,6 +34,23 @@ final class DefaultTimerViewModel: ObservableObject, TimerViewModel {
 
 // MARK: - Input
 extension DefaultTimerViewModel {
+    func configureNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { settings in
+            guard (settings.authorizationStatus == .authorized) ||
+                  (settings.authorizationStatus == .provisional) else {
+                self.requestAuthorization()
+                return
+            }
+            
+            if settings.alertSetting == .enabled {
+                // Schedule an alert-only notification.
+            } else {
+                // Schedule a notification with a badge and sound.
+            }
+        }
+    }
+    
     func startTimer() {
         isTimerRunning.toggle()
     }
@@ -39,6 +58,21 @@ extension DefaultTimerViewModel {
     func tick() {
         if isTimerRunning && remainingTime > 0 {
             remainingTime -= 1
+        }
+    }
+}
+
+// MARK: - Private
+private extension DefaultTimerViewModel {
+    func requestAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge, .provisional]) { granted, error in
+            if granted {
+                // Configure sended Notification
+            } else if let error = error {
+                #if DEBUG
+                print(String(describing: error))
+                #endif
+            }
         }
     }
 }
