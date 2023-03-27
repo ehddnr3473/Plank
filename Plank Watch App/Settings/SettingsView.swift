2022c13f42674fecb50fc5faf8bc5f9a1f31bb0c
Donguk Kim <ehddnr73@naver.com>
2023-03-28 02:40:01 +0900

    SettingsView에 @ObservedObject로서 SettingsState 타입의 프로퍼티 정의
    - Stepper의 value가 변경됨에 따라, SettingsManager를 통해 SettingsState 저장

--- Plank Watch App/Settings/SettingsView.swift
+++ Plank Watch App/Settings/SettingsView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State var value: Float
+    @ObservedObject var settingsState: SettingsState
     
     var body: some View {
-        Stepper(String(Int(value)), value: $value)
+        Stepper(String(Int(settingsState.time)), value: $settingsState.time)
             .digitalCrownRotation(
-                $value,
+                $settingsState.time,
                 from: 0,
                 through: 200,
                 by: 1,
@@ -21,11 +21,17 @@ struct SettingsView: View {
                 isContinuous: true,
                 isHapticFeedbackEnabled: true
             )
+            .onChange(of: settingsState.time) { _ in
+                SettingsManager.save(
+                    settingsState,
+                    saveName: "TIME"
+                )
+            }
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView(value: 60)
+        SettingsView(settingsState: SettingsState())
     }
 }
